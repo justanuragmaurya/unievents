395548a61b5046e28e0fbcb8e1ececc260874672
justanuragmaurya <justanuragmaurya@gmail.com>
2024-08-21 23:36:39 +0530

    added events page using dummy data along with searchbar(search function not implemented)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import BlurFade from "@/components/magicui/blur-fade";
-import { cn } from "@/lib/utils";
-import GridPattern from "@/components/magicui/grid-pattern";
 import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
 
 export default function Home() {
   return (
@@ -9,10 +9,11 @@ export default function Home() {
       <div>
         <BlurFade delay={0.25} inView>
           <div className="flex flex-col justify-center text-center items-center my-12 md:my-24 lg:my-48 w-full text-[#fafafa] px-4 md:px-8">
-            <div>
-              <h2 className="bg-[#1a1a1a] px-5 py-1 my-2 rounded-full border-[0.5px] border-[#3a3a3a] shadow-md">
-                ✨ All events a click away
-              </h2>
+            <div className="bg-gradient-to-br from-[#fafafa] to-[#cfcfcf] rounded-full m-2">
+              <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                <span>✨ Introducing UniEvents</span>
+                <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+              </AnimatedShinyText>  
             </div>
             <h2 className="text-3xl md:text-4xl lg:text-6xl font-semibold">
               Discover Campus Life <br />
